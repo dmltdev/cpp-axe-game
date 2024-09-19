85f4baa81d0e03546fc2dd9de0a542041217ef70
dmltdev <hone.domitoro@gmail.com>
2024-09-19 14:45:14 +0300

    chore: initial commit

---
+++ axe_game.cpp
@@ -0,0 +1,11 @@
+# include "raylib.h"
+
+int main() 
+{
+    // int width = 350;
+    // InitWindow(width, 300, "Anna Banana");
+
+    float root_beer = 1.99;
+    double cheese_burger = 5.99;
+    bool shouldHaveLunch = true;
+}
