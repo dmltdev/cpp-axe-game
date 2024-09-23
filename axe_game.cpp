9abe7824ef6146d67c0acad73de7f1165786d1bf
dmltdev <hone.domitoro@gmail.com>
2024-09-23 10:22:40 +0300

    feat: add window boundaries for circle keydown events

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,11 +1,35 @@
-# include "raylib.h"
+#include "raylib.h"
+#include <stdio.h>
 
 int main() 
 {
-    // int width = 350;
-    // InitWindow(width, 300, "Anna Banana");
+    int width{350};
+    int height{200};
+    InitWindow(width, height, "Axe Game");
 
-    float root_beer = 1.99;
-    double cheese_burger = 5.99;
-    bool shouldHaveLunch = true;
+   /* The lines `int circle_x{width/2};` and `int circle_y{height/2};` are initializing the variables
+   `circle_x` and `circle_y` with the half of the `width` and `height` respectively. This positions
+   the circle at the center of the window initially. */
+    int circle_x{width/2};
+    int circle_y{height/2};
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) 
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+
+        if (IsKeyDown(KEY_D) && circle_x < width)
+        {
+            circle_x = circle_x + 10;
+        }
+        if (IsKeyDown(KEY_A) && circle_x > 0)
+        {
+            circle_x = circle_x - 10;
+        }
+
+        EndDrawing();
+    }
 }
